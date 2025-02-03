6c06d40326b7f34dd2d19c58d877545f6273a486
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-03 17:36:08 -0300

    adding sidebar to app eda project

--- appeda.py
+++ appeda.py
@@ -7,4 +7,12 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 st.title('AI Assistant for Data Science')
 st.header('Exploratory Data Analysis Part')
 st.subheader('Solution')
-st.write('Hello, I am AI Assistant and I am here to help you with data science projects')
+st.write('''Hello, I'm AI Assistant and I'm here to help you with data science projects''')
+
+with st.sidebar:
+    st.write('*Your Data Science adventure begins with a CSV file.*')
+    st.caption('**You may already know that every exciting data science journey starts with a dataset**')
+
+    st.divider()
+
+    st.caption("<p style = 'text-align:center'>Made By Me</p>", unsafe_allow_html=True)
