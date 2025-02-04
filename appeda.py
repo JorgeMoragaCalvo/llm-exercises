13bf9473052d44c16d8408468811c7b5d1158b63
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-04 00:08:36 -0300

    adding session state

--- appeda.py
+++ appeda.py
@@ -18,8 +18,19 @@ with st.sidebar:
 
     st.caption("<p style = 'text-align:center'>Made By Me</p>", unsafe_allow_html=True)
 
-if st.button("Let's get started"):
+# Button are not stateful. You can use the st.session_state object to create and manage session state varibles
+
+# Initialise the key in session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False}
+
+def clicked(button):
+    st.session_state.clicked[button]=True
+
+st.button("Let's get started", on_click=clicked, args=[1])
+
+if st.session_state.clicked[1]:
     st.header('Exploratory Data Analysis Part')
     st.subheader('Solution')
     
-user_csv = st.file_uploader("Upload your file here", type="csv")
+    user_csv = st.file_uploader("Upload your file here", type="csv")
--- simplechatbot.py
+++ simplechatbot.py
@@ -1,30 +1,75 @@
-import streamlit as st
-from langchain_ollama import ChatOllama
-
-st.title("🧠 This a Simple Chat Application with Ollama/DeepSeek and Langchain!")
-
-with st.form("llm-form"):
-    text = st.text_area("Enter your question:")
-    submit = st.form_submit_button("Submit")
-
-def generate_response(input_text):
-    model = ChatOllama(model="deepseek-r1:1.5b", base_url="http://localhost:11434/")
-
-    response = model.invoke(input_text)
-
-    return response.content
-
-if "chat_history" not in st.session_state:
-    st.session_state['chat_history'] = []
-
-if submit and text:
-    with st.spinner("Generating response..."):
-        response = generate_response(text)
-        st.session_state['chat_history'].append({"user": text, "ollama": response})
-        st.write(response)
-
-st.write("## Chat History")
-for chat in reversed(st.session_state['chat_history']):
-    st.write(f"** 🧑 User**: {chat['user']}")
-    st.write(f"**🧠 Assistant**: {chat['ollama']}")
-    st.write("---")
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import streamlit as st\n",
+    "from langchain_ollama import ChatOllama"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "2025-02-03 14:24:14.271 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.274 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.275 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.276 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.278 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.279 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.281 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.282 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.283 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.284 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.285 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.286 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.287 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.289 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.289 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.290 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.291 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.293 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n",
+      "2025-02-03 14:24:14.294 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.\n"
+     ]
+    }
+   ],
+   "source": [
+    "st.title(\"🧠 Make Your Own Chat Application with Ollama and Langchain!!!\")\n",
+    "st.write(\"GET MORE INFO @ KGP Talkie: https://www.youtube.com/kgptalkie\")\n",
+    "\n",
+    "with st.form(\"llm-form\"):\n",
+    "    text = st.text_area(\"Enter your question: \")\n",
+    "    submit = st.form_submit_button(\"Submit\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": ".venv",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
