2e609658f559d9e60f406f351a0a7999e39efc91
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-04 00:38:48 -0300

    adding LLM model

--- appeda.py
+++ appeda.py
@@ -4,15 +4,14 @@ import pandas as pd
 from langchain_ollama import OllamaLLM
 from langchain_experimental.agents import create_pandas_dataframe_agent
 
+llm = OllamaLLM(model="deepseek-r1:1.5b", base_url="http://localhost:11434/")
+
 st.title('AI Assistant for Data Science 🤖')
 st.write('''Hello :hand:, I'm AI Assistant and I'm here to help you with data science projects''')
 
 with st.sidebar:
     st.write('*Your Data Science adventure begins with a CSV file.*')
     st.caption('**You may already know that every exciting data science journey starts with a dataset**')
-
-    with st.expander('an expander'):
-        st.write('Expander text')
 
     st.divider()
 
@@ -34,3 +33,11 @@ if st.session_state.clicked[1]:
     st.subheader('Solution')
     
     user_csv = st.file_uploader("Upload your file here", type="csv")
+
+    if user_csv is not None:
+        user_csv.seek(0)
+        df = pd.read_csv(user_csv, low_memory=False)
+
+with st.sidebar:
+    with st.expander('What are the steps of EDA'):
+        st.write(llm('What are the steps of EDA'))
